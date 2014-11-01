31355177a98c2fd5eda9cc84cf678e0c47720b10
Pablo Barrio <p.barrio.lopez@gmail.com>
2014-11-01 18:11:07 +0000

    Fixed some environment variables in the install script for Hornet.

--- install_hornet.sh
+++ install_hornet.sh
@@ -3,11 +3,11 @@
 # Insert the following lines into your .bashrc
 #   export ARMCI_HOME=/home/pablo/Applications/ga-v5.3/installTree
 #   export PARMETIS_HOME=/usr/
-#   export LD_LIBRARY_PATH=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server:$ARMCI_HOME/lib:$LD_LIBRARY_PATH
+#   export ROSE_HOME=/home/pablo/Applications/rose-v0.9.5a/installTree/
+#   export BOOST_HOME=/home/pablo/Applications/boost-v1.47.0/installTree/
+#   export LD_LIBRARY_PATH=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server:$ARMCI_HOME/lib:$ROSE_HOME/lib:$BOOST_HOME/lib:$LD_LIBRARY_PATH
 
-export ROSE_HOME=/home/pablo/Applications/rose-v0.9.5a/installTree/
-export BOOST_HOME=/home/pablo/Applications/boost-v1.47.0/installTree/
-export MPI_HOME=/usr/bin/
+export MPI_HOME=/usr/
 
 cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/usr $PWD
 make
