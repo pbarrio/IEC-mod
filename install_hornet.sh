d7256620846fd5e43c6a1cf407ddf115240adbfc
Pablo Barrio <p.barrio.lopez@gmail.com>
2017-04-05 23:32:48 +0100

    Improve IEC install script for hornet (Ubuntu 14.04)

--- install_hornet.sh
+++ install_hornet.sh
@@ -1,21 +1,20 @@
 #! /bin/bash
 
 # Insert the following lines into your .bashrc
-#   export ARMCI_HOME=/home/pablo/Applications/ga-v5.3/installTree
-#   export PARMETIS_HOME=/usr/
 #   export ROSE_HOME=/home/pablo/Applications/rose-v0.9.5a/installTree/
-#   export BOOST_HOME=/home/pablo/Applications/boost-v1.47.0/installTree/
-#   export LD_LIBRARY_PATH=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server:$ARMCI_HOME/lib:$ROSE_HOME/lib:$BOOST_HOME/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server:$LD_LIBRARY_PATH #:$ARMCI_HOME/lib:$ROSE_HOME/lib:$BOOST_HOME/lib
 
 export MPI_HOME=/usr/
 
-mkdir -p build
+mkdir -p buildTree
+mkdir -p installTree
 
-cd build
-cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=/usr .. \
-	  -DARMCI_HOME=/home/pablo/Applications/ga-v5.3/installTree \
-	  -DBOOST_HOME=/home/pablo/Applications/boost-v1.47.0/installTree/ \
-	  -DPARMETIS_HOME=/usr/
+cd buildTree
+cmake -Wno-dev -DCMAKE_INSTALL_PREFIX=../installTree .. \
+    -DARMCI_HOME=/home/pablo/Applications/ga-v5.3/installTree \
+    -DBOOST_HOME=/home/pablo/Applications/boost-v1.47.0/installTree/ \
+    -DPARMETIS_HOME=/usr/ \
+    -DROSE_HOME=/home/pablo/Applications/rose-v0.9.5a/installTree/
 make -j4
-sudo make install
+make install
 cd ..
